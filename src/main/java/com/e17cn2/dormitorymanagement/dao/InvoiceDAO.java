f38928632c591fe9bf19b1373887adad1642de17
lenam30101999 <levunam30101999@gmail.com>
2020-06-30 23:29:47 +0700

    invoiceDAO

--- src/main/java/com/e17cn2/dormitorymanagement/dao/InvoiceDAO.java
+++ src/main/java/com/e17cn2/dormitorymanagement/dao/InvoiceDAO.java
@@ -7,7 +7,14 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class InvoiceDAO {
-    //select * from cnpm.tbldonghodien where tblPhongid = ? order by (ngayLaySo) DESC limit 2;
-    //select * from cnpm.tbldonghonuoc where tblPhongid = ? order by (ngayLaySo) DESC limit 2;
+    //select * from cnpm.tbldonghodien where tblPhongid IN 
+    //(SELECT tblPhongid FROM cnpm.tblgiuong WHERE id IN (SELECT tblGiuongid FROM tblgiuongdat WHERE id = 1)) 
+    //order by (ngayLaySo) DESC limit 2;
+    
+    //select * from cnpm.tbldonghonuoc where tblPhongid IN 
+    //(SELECT tblPhongid FROM cnpm.tblgiuong WHERE id IN (SELECT tblGiuongid FROM tblgiuongdat WHERE id = 1)) 
+    //order by (ngayLaySo) DESC limit 2;
+    //
+    
     //
 }
