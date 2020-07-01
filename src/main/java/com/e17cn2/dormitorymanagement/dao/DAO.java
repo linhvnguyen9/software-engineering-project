a89ff585ef422eaf3b91a59785c732d2b8952a44
lenam30101999 <levunam30101999@gmail.com>
2020-07-01 13:35:38 +0700

    modify DAO

--- src/main/java/com/e17cn2/dormitorymanagement/dao/DAO.java
+++ src/main/java/com/e17cn2/dormitorymanagement/dao/DAO.java
@@ -8,12 +8,12 @@ public class DAO {
 
     public DAO(){
         if(con == null){
-            String dbUrl = "";
+            String dbUrl = "jdbc:mysql://localhost:3306/cnpm";
             String dbClass = "com.mysql.jdbc.Driver";
 
             try {
                 Class.forName(dbClass);
-                con = DriverManager.getConnection (dbUrl, "root", "");
+                con = DriverManager.getConnection (dbUrl, "root", "nam30101999");
             }catch(Exception e) {
                 e.printStackTrace();
             }
--- src/main/java/com/e17cn2/dormitorymanagement/dao/InvoiceDAO.java
+++ src/main/java/com/e17cn2/dormitorymanagement/dao/InvoiceDAO.java
@@ -7,14 +7,35 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class InvoiceDAO {
+    //Lay 2 chi so dien gan nhat
     //select * from cnpm.tbldonghodien where tblPhongid IN 
-    //(SELECT tblPhongid FROM cnpm.tblgiuong WHERE id IN (SELECT tblGiuongid FROM tblgiuongdat WHERE id = 1)) 
+    //(SELECT tblPhongid FROM cnpm.tblgiuong WHERE id IN (SELECT tblGiuongid FROM tblgiuongdat WHERE id = ?)) 
     //order by (ngayLaySo) DESC limit 2;
     
+    //Lay 2 chi so nuoc gan nhat
     //select * from cnpm.tbldonghonuoc where tblPhongid IN 
-    //(SELECT tblPhongid FROM cnpm.tblgiuong WHERE id IN (SELECT tblGiuongid FROM tblgiuongdat WHERE id = 1)) 
+    //(SELECT tblPhongid FROM cnpm.tblgiuong WHERE id IN (SELECT tblGiuongid FROM tblgiuongdat WHERE id = ?)) 
     //order by (ngayLaySo) DESC limit 2;
-    //
     
-    //
+    //tim giuong dat chua len hoa don thang nay
+    //select * from tblgiuong 
+    //where id IN 
+    //(select tblGiuongid from tblGiuongDat where id NOT IN (select tblGiuongDatid from tblhoadon where DATE_FORMAT(tblhoadon.ngayLap, '%y-%m') >= DATE_FORMAT(current_timestamp, '%y-%m')));
+    
+    
+    //lay thong tin phong cua giuongdat
+    //select * from tblPhong where id IN(select tblPhongid from tblgiuong,tblgiuongdat where tblgiuong.id = tblgiuongdat.tblGiuongid);
+    
+    //Lay thong tin cua giuong
+    //select * from tblgiuong 
+    //where id IN (select tblGiuongid from tblGiuongDat);
+    
+    //Lay thong tin sinh vien
+    //select * from tblsinhvien 
+    //where id IN (select tblHopDong.tblSinhVienid FROM tblHopDong,tblGiuongDat where tblHopDong.id = tblGiuongDat.tblHopDongid and tblGiuongDat.id = ?);
+    
+    //Lay tong so tien con no cua cac hoa don truoc
+    //select sum(soTienConNo) from tblhoadon where tblGiuongDatid = ?;
+    
+    //Insert into tblhoadon values(?,?,?,?,?,?,?);
 }
